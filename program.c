6e4de65317fdf77ac9ffd5a0559501ffd578855a
Ivan Grishaev <ivan@grishaev.me>
2016-09-06 17:14:08 +0300

    updated

--- program.c
+++ program.c
@@ -12,16 +12,16 @@ int main(){
 
     if (0 == pid){
         // child
-        close(pipefd[0]); // reading end of the pipe
-        dup2(pipefd[1], 1); // stdout ---> pipe writing end
-        execlp("ls", "ls", NULL);
+        close(pipefd[0]);
+        dup2(pipefd[1], 1);
+        execl("/bin/ls", "ls", "-l", (char *) NULL);
 
     }
     else {
         // parent
-        close(pipefd[1]); // writing end of the pipe
-        dup2(pipefd[0], 0);  // stdin ----> pipe reading end
-        execlp("wc", "wc", NULL);
+        close(pipefd[1]);
+        dup2(pipefd[0], 0);
+        execl("/usr/bin/grep", "grep", "file", (char *) NULL);
     }
 
     return 0;
