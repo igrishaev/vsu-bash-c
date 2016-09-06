80d416a25d26d3d12431d86ce4ba381616a295f1
Ivan Grishaev <ivan@grishaev.me>
2016-09-06 17:07:06 +0300

    updated

--- program.c
+++ program.c
@@ -3,51 +3,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int main(){
+    int pipefd[2];
+    pipe(pipefd);
 
-void read_pipe (int file) {
-    FILE *stream;
-    int c;
-    stream = fdopen (file, "r");
-    while ((c = fgetc (stream)) != EOF)
-        putchar (c);
-    fclose (stream);
-}
-
-
-void write_pipe (int file) {
-    FILE *stream;
-    stream = fdopen (file, "w");
-    fprintf (stream, "foo\n");
-    fprintf (stream, "bar\n");
-    fclose (stream);
-}
-
-
-int main (void) {
-    pid_t pid, pid_master;
-    int mypipe[2];
-
-    // create a pipe
-    if (pipe (mypipe) != 0) {
-        printf("Pipe error\n");
-        return -1;
-    }
-
-    // fork the process
-    pid_master = getpid();
+    pid_t pid;
     pid = fork();
 
-    if ((pid_t) 0 == pid) {
-        // slave
-        close (mypipe[1]);
-        read_pipe (mypipe[0]);
-        return 0;
+    if (0 == pid){
+        // child
+        close(pipefd[0]); // reading end of the pipe
+        dup2(pipefd[1], 1); // stdout ---> pipe writing end
+        execlp("ls", "ls", NULL);
+
+    }
+    else {
+        // parent
+        close(pipefd[1]); // writing end of the pipe
+        dup2(pipefd[0], 0);  // stdin ----> pipe reading end
+        execlp("wc", "wc", NULL);
     }
 
-    else {
-        // master
-        close (mypipe[0]);
-        write_pipe (mypipe[1]);
-        return 0;
-    }
+    return 0;
 }
