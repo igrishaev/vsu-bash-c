4535a43e7b938b660458fa7c51d9a6e9ea4e677a
Ivan Grishaev <ivan@grishaev.me>
2016-09-07 11:31:15 +0300

    prompt updated

--- prompt.sh
+++ prompt.sh
@@ -1,28 +1,54 @@
 #!/bin/bash
 
-PS3='Please enter your choice: '
-options=("Option 1" "Input manualy" "Quit")
-select opt in "${options[@]}"
-do
-    case $opt in
-        "Option 1")
-            echo "you chose choice 1"
+save_commands() {
+    echo "CMD1 = $1" > Definitions
+    echo "CMD2 = $2" >> Definitions
+    echo "Commands saved."
+}
+
+top_menu() {
+    echo "Select an option:"
+    echo "1) Choose default commands"
+    echo "2) Input commands manualy"
+    echo "*) Quit"
+}
+
+sec_menu() {
+    echo "Select a command pair:"
+    echo "1) who | wc -l"
+    echo "2) ls -l | head -n 4"
+    echo "*) Quit"
+}
+
+main() {
+    top_menu
+    read opt1
+    case $opt1 in
+        1 )
+            sec_menu
+            read opt2
+            case $opt2 in
+                1)
+                    save_commands "who" "wc -l"
+                    ;;
+                2)
+                    save_commands "ls -l" "head -n 4"
+                    ;;
+                *)
+                    echo "Bye!"
+                    ;;
+            esac
             ;;
-        "Input manualy")
+        2 )
             echo "Enter command #1:"
             read CMD1
             echo "Enter command #2:"
             read CMD2
-
-            echo "CMD1 = $CMD1" > Definitions
-            echo "CMD2 = $CMD2" >> Definitions
-
-            echo "Commands saved."
-            break
+            save_commands "$CMD1" "$CMD2"
             ;;
-        "Quit")
-            break
+        * ) echo "Bye!"
             ;;
-        *) echo invalid option;;
     esac
-done
+}
+
+main
